d095330e244a527fec36410c2bfef882d78ed87c
ABHISHEK UPADHYAY <abhisheku722@gmail.com>
2021-06-29 20:46:39 +0530

    new changes HungryClashers and waysmall

--- scripts.js
+++ scripts.js
@@ -1,14 +1,34 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
 
-  function display() {
-    var x = document.getElementsByClassName("free_text");
-    x.style.display = "block";
-  };
+function display() {
+  var x = document.getElementsByClassName("free_text");
+  x.style.display = "block";
+};
+
+// function download() {
+//   var bt = document.getElementsByClassName("modal");
+//   window.innerHTML = bt.style.display = "block";
+// }
+
+// var modal = document.getElementById("myModal");
+// var btn = document.getElementById("myBtn");
+// var span = document.getElementByclassName("close")[0];
+// btn.onclick = function () {
+//   modal.style.display = "block";
+// }
+// span.onclick = function () {
+//   modal.style.display = "none";
+// }
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
