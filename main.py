357b0ec3f8acd63a4718e3f4ac4d926b5f6f2f15
ABHISHEK UPADHYAY <abhisheku722@gmail.com>
2021-07-05 19:44:51 +0530

    Testing
    Testing the code.

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import pyqrcode
 import png
 from pyqrcode import QRCode
 import pandas as pd
-s = pd.read_csv("contactdetails.csv")
-print(s)
-import ContactDetails
+# s = pd.read_csv("contactdetails.csv")
+# print(s)
+# import ContactDetails
 # from ContactQR import ContactDetails
 # import ContactQR
+s = input("Enter text to create qr:\n")
 
 url = pyqrcode.create(s)
 # url = pyqrcode.create(ContactQR.ContactDetails)
