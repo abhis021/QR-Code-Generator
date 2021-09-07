096a928d6000af5c811f1a56bafa2bf1a5ce2f67
NathShubham28 <nathshubham69@gmail.com>
2021-09-07 17:11:39 +0530

    Web Page

--- scripts.js
+++ scripts.js
@@ -12,23 +12,3 @@ function display() {
   var x = document.getElementsByClassName("free_text");
   x.style.display = "block";
 };
-
-// function download() {
-//   var bt = document.getElementsByClassName("modal");
-//   window.innerHTML = bt.style.display = "block";
-// }
-
-// var modal = document.getElementById("myModal");
-// var btn = document.getElementById("myBtn");
-// var span = document.getElementByclassName("close")[0];
-// btn.onclick = function () {
-//   modal.style.display = "block";
-// }
-// span.onclick = function () {
-//   modal.style.display = "none";
-// }
-// window.onclick = function (event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
